b1696fd11c9f01e5f0713f78cc5d1e92383d7720
vishal <vishalgupta013@gmail.com>
2017-06-14 18:36:49 +0530

    spring project setuped

---
+++ src/main/java/com/todo/ToDoApplication.java
@@ -0,0 +1,15 @@
+package com.todo;
+
+
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+
+@SpringBootApplication
+public class ToDoApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ToDoApplication.class, args);  
+	}
+
+}
