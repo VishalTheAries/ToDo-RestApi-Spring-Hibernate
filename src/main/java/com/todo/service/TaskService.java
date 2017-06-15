6c5018a473f84edd82888f17d6400458cc4c0e07
vishal <vishalgupta013@gmail.com>
2017-06-15 23:46:43 +0530

    Task Service Created

---
+++ src/main/java/com/todo/service/TaskService.java
@@ -0,0 +1,42 @@
+package com.todo.service;
+
+import com.todo.models.Task;
+import com.todo.dao.jpa.TaskRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+
+@Service
+public class TaskService {
+	
+	//To do
+    @Autowired
+    private TaskRepository taskRepository;
+    
+    public TaskService() {
+    }
+    
+
+    public Task createtask(Task task) {
+    	System.out.println("before save");
+        return taskRepository.save(task);
+    }
+
+    public Task getTask(long id) {
+        return taskRepository.findOne(id);
+    }
+
+    public void updateTask(Task task) {
+    	taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.delete(id);
+    }
+
+    public Iterable<Task> getAllTasks() {
+    	Iterable pageOfTask = taskRepository.findAll();
+        return pageOfTask;
+    }
+    
+}
