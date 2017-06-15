d97ad4e6d3f02c10a9eefc2197345da5dce70735
vishal <vishalgupta013@gmail.com>
2017-06-15 23:47:24 +0530

    Task Repository Created

---
+++ src/main/java/com/todo/dao/jpa/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.todo.dao.jpa;
+import com.todo.models.Task;
+import java.util.Date;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Long>{
+//    Task findTaskByDate(Date date);
+}
