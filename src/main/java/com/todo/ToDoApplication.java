76c53f6dc77506b5498e52d3077aaa47ec3a9a95
vishal <vishalgupta013@gmail.com>
2017-06-15 23:46:05 +0530

    SpringBootServletInitializer

--- src/main/java/com/todo/ToDoApplication.java
+++ src/main/java/com/todo/ToDoApplication.java
@@ -1,15 +1,28 @@
 package com.todo;
 
 
-import org.springframework.boot.SpringApplication;  
-import org.springframework.boot.autoconfigure.SpringBootApplication;  
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class ToDoApplication {
-
+@ComponentScan(basePackages = "com.todo")
+@EnableJpaRepositories("com.todo.dao.jpa") 
+@EnableAutoConfiguration
+public class ToDoApplication extends SpringBootServletInitializer{
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(ToDoApplication.class, args);  
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ToDoApplication.class);
+    }
 
 }
